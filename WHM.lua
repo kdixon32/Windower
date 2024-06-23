b9d4b618b50d032beb3a33e767e7913f9d365f6f
kdixon32 <77586681+kdixon32@users.noreply.github.com>
2024-06-23 11:57:39 -0400

    macro book fix

--- WHM.lua
+++ WHM.lua
@@ -387,6 +387,6 @@ end
 -- Select default macro book on initial load or subjob change.
 function select_default_macro_book()
     -- Default macro set/book
-    set_macro_page(4, 14)
+    set_macro_page(1, 1)
 end
 
