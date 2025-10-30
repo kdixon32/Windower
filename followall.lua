e58fdf2ff27db3dd3bf79aeafb61cce18370deac
kdixon32 <77586681+kdixon32@users.noreply.github.com>
2025-10-30 18:13:33 -0400

    Script and Addon Updates

---
+++ followall.lua
@@ -0,0 +1,19 @@
+_addon.name = 'followall'
+_addon.author = 'Chocopon'
+_addon.version = '1.0'
+_addon.commands = {'followall'}
+
+windower.register_event('addon command', function(...)
+    -- Optional: allow manual names: //followall SomeName
+    local args = {...}
+    local target = args[1]
+    if not target or target == '' then
+        local me = windower.ffxi.get_player()
+        if not me or not me.name then
+            windower.add_to_chat(123, '[followall] Could not determine player name.')
+            return
+        end
+        target = me.name
+    end
+    windower.send_command(('send @others input /follow %s'):format(target))
+end)
